e43b17aac54cc2e94672ec20384ae8c1c847eb0c
iLighty33 <iLighty33@gmail.com>
2022-06-02 22:18:25 +0500

    Add project files.

---
+++ DynamicMemory/DynamicArrayClass.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <cstddef>
+#include <new>
+#include <vector>
+
+using namespace std;
+
+class DynamicArray {
+public:
+	
+	// A.Constructor
+	DynamicArray() {};
+	DynamicArray(int size, int* data);
+	DynamicArray(DynamicArray&& other);
+	DynamicArray(const DynamicArray& other);
+
+	// B. Destructor
+	~DynamicArray();
+
+	// C. Operator=
+	DynamicArray& operator= (const DynamicArray& other);
+	DynamicArray& operator= (DynamicArray&& other);
+
+	// D. At
+	int& at(int pos);
+	const int& at(int pos) const;
+
+	// E. Operator{]
+	int& operator[] (int pos);
+	const int& operator[] (int pos) const;
+
+	// F. Front
+	int & front () {
+		return data_[0];
+	}
+	const int& front() const {
+		return data_[0];
+	}
+
+	// G. Back
+	int& back();
+	const int& back() const;
+
+	// H. Data
+	int* data();
+	const int* data() const;
+
+	// I. Capacity
+	int capacity() const;
+
+	// J. Empty
+	bool empty() const {
+		if (size_ > 0) {
+			return false;
+		}
+		else
+			return true;
+	}
+
+	// K. Size
+	int size() const {
+		return size_;
+	}
+
+	// L. Reserve
+	void reserve(int newCapacity) {
+		if (newCapacity > capacity_)
+			;
+	}
+	
+	// M. Capacity
+	int capacity() const {
+		if (capacity_ == size_)
+			;
+	}
+
+	// N. Shrink_to_fit
+	// O. Clear
+	void clear() {
+		delete[] data_;
+		size_ = 0;
+	}
+
+	// P. Insert
+	// Q. Erase
+	// R. Push_back
+	void push_back(int value) {
+		int* tmp = new int[size_ + 1];
+
+		for (int i = 0; i < size_; i++) {
+			tmp[i] = this->data_[i];
+		}
+		size_ += 1;
+		for (int i = 0; i < this->size_ - 1; i++) {
+			this->data_[i] = tmp[i];
+		}
+		data_[size_ - 1] = value;
+		delete[] tmp;
+	}
+
+	// T. Pop_back
+	// U. Resize
+	// V. Swap
+	void swap(DynamicArray& other, DynamicArray& other2) {
+		DynamicArray tmp;
+		tmp = other;
+		other = other2;
+		other2 = tmp;
+	}
+
+	// W. Swaps the contents
+	// X. Operator==
+
+private:
+	int* data_;
+	int size_;
+	int capacity_;
+};
+
+int main() {
+
+
+
+	return 0;
+}
